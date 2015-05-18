6378b48c102b1279c9402de3e76437e09ec93d7c
furkanyucel <furkanyucel@10.53.167.83>
2015-05-18 10:32:14 +0200

    gui wird bearbeitet

--- src/fh/prog/lab/it/gui/maingui.java
+++ src/fh/prog/lab/it/gui/maingui.java
@@ -1,12 +1,25 @@
 package fh.prog.lab.it.gui;
 
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.sun.medialib.mlib.Image;
 
 	public class maingui {
 	private String title = "Sharknado Synergy Base v0.1";
+	private Image vertrieb,inventur,werbung,statistik,managment;
+	
 		public maingui(){
 			JFrame frame = new JFrame(title);
+			JPanel panel = new JPanel();
+			LayoutManager layout = new GridLayout(2,2);
+			
+			
 			frame.setSize(800, 600);
+			panel.setLayout(layout);
 			
 		}
 	

8b7f5686dd3b33f08f07aaac5f0ff01594697cb7
furkanyucel <furkanyucel@10.53.167.83>
2015-05-18 09:48:05 +0200

    aa

--- src/fh/prog/lab/it/gui/maingui.java
+++ src/fh/prog/lab/it/gui/maingui.java
@@ -2,6 +2,12 @@ package fh.prog.lab.it.gui;
 
 import javax.swing.JFrame;
 
-public class maingui {
+	public class maingui {
+	private String title = "Sharknado Synergy Base v0.1";
+		public maingui(){
+			JFrame frame = new JFrame(title);
+			frame.setSize(800, 600);
+			
+		}
 	
 }

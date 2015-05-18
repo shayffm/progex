5848f353cd3b381437fc06526ff3c6f47ab2a8a4
seamuswo <seamuswo@192.168.249.107>
2015-05-18 09:49:12 +0200

    Controller erstellt

---
+++ src/fh/prog/lab/it/controller/Controller.java
@@ -0,0 +1,14 @@
+package fh.prog.lab.it.controller;
+import java.awt.event.*;
+
+
+
+public class Controller implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

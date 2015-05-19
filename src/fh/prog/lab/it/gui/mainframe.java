796dac6f43033abd0ed05ae326936d495fc84779
furkanyucel <mfy1992@live.de>
2015-05-20 00:17:09 +0200

    mainframe wurde in runhere.java datei gestartet:D

--- src/fh/prog/lab/it/gui/mainframe.java
+++ src/fh/prog/lab/it/gui/mainframe.java
@@ -7,12 +7,14 @@ import javax.swing.JFrame;
 
 public class mainframe {
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	
+	private String title = "Sharknado Beta 0.1";
 	public mainframe(){
 		double width = screenSize.getWidth();
 		double height = screenSize.getHeight();
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame(title);
 		frame.setSize((int)width, (int)height);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
 
 }
--- src/fh/prog/lab/it/samples/run/runHere.java
+++ src/fh/prog/lab/it/samples/run/runHere.java
@@ -5,6 +5,7 @@ package fh.prog.lab.it.samples.run;
 
 import java.sql.SQLException;
 
+import fh.prog.lab.it.gui.mainframe;
 import fh.prog.lab.it.samples.dbServices.DBServicesInvoker;
 
 /**
@@ -18,7 +19,7 @@ public class runHere {
 	 */
 	public static void main(String[] args) {
 
-		
+		// new mainframe();
 		DBServicesInvoker dbService = new DBServicesInvoker();
 		try {
 			

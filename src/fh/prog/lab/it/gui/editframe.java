842cb8ca91efeb3a9c176cf0d7ef77dcba002dd6
furkanyucel <furkanyucel@furkans-mbp.local>
2015-07-01 04:11:19 +0200

    a

---
+++ src/fh/prog/lab/it/gui/editframe.java
@@ -0,0 +1,26 @@
+package fh.prog.lab.it.gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class editframe extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel panel;
+	JLabel label;
+	public editframe(){
+		panel = new JPanel(new GridLayout(2, 2));
+		label = new JLabel("Das ist ein Test");
+		add(panel);
+		panel.add(label);
+		setTitle("EditierFenster");
+		setLocationRelativeTo(null);
+		setSize(300, 500);
+		setVisible(true);
+	}
+}
---
+++ src/src/fh/prog/lab/it/samples/run/runHere.java
@@ -0,0 +1,33 @@
+package src.fh.prog.lab.it.samples.run;
+
+
+import java.sql.SQLException;
+
+import fh.prog.lab.it.gui.Controller;
+import fh.prog.lab.it.gui.mainframe;
+import fh.prog.lab.it.gui.menubar;
+import fh.prog.lab.it.samples.dbServices.DBServicesInvoker;
+
+public class runHere {
+
+	/**
+	 * @param args
+	 * @throws SQLException 
+	 */
+	public static void main(String[] args) throws SQLException {
+		
+		new Controller();
+		DBServicesInvoker dbService = new DBServicesInvoker();
+		try {
+			
+			dbService.getDB();
+		}catch (SQLException sqlex) {
+			
+			System.out.println("SQL Error : " + sqlex.getMessage());
+		}  catch (Exception e) {
+			
+			System.out.println("SQL Error : " + e.getMessage());
+		}
+	}
+
+}
